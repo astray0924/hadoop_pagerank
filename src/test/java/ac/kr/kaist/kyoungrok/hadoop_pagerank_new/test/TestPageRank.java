671a46b4b9672f9ac3c676ac98eb42f348d7c0d1
kyoungrok <kyoungrok.jang@gmail.com>
2014-04-23 10:41:27 +0900

    Graph까지는 검증 완료

--- src/test/java/ac/kr/kaist/kyoungrok/hadoop_pagerank_new/test/TestPageRank.java
+++ src/test/java/ac/kr/kaist/kyoungrok/hadoop_pagerank_new/test/TestPageRank.java
@@ -1,10 +1,7 @@
 package ac.kr.kaist.kyoungrok.hadoop_pagerank_new.test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.hadoop.io.FloatWritable;
